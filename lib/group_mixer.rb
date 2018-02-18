6b67374fada21cb4282c1897d9ac20651fdd6dca
iwtn <ganta.viii@gmail.com>
2018-02-18 23:50:10 +0900

    not fix

--- lib/group_mixer.rb
+++ lib/group_mixer.rb
@@ -7,14 +7,36 @@ module GroupMixer
   end
 
   def self.by_group_size(people, past_set, group_size, is_separate_reminders = false)
-    Mixer.new(people, past_set, group_size, is_separate_reminders).execute
+    if group_size.zero?
+      raise ZeroGroupSize, 'group_size must be a number greater than 1'
+    end
+    max_member_size = (people.size.to_f / group_size).ceil
+    rest = people.size % max_member_size
+    groups = make_groups(rest, group_size, max_member_size, is_separate_reminders)
+    Mixer.new(people, past_set, groups, is_separate_reminders).execute
   end
 
   def self.by_member_size(people, past_set, max_member_size, is_separate_reminders = false)
     if max_member_size.to_f < 1
       raise ZeroMaxMemberSize, 'max_member_size must be a number greater than 1'
     end
-    group_size = (people.size / max_member_size.to_f).ceil
-    Mixer.new(people, past_set, group_size, is_separate_reminders).execute
+    rest = people.size % max_member_size
+    group_size = (people.size.to_f / max_member_size).ceil
+    groups = make_groups(rest, group_size, max_member_size, is_separate_reminders)
+    Mixer.new(people, past_set, groups, is_separate_reminders).execute
+  end
+
+  private
+
+  def self.make_groups(rest, group_size, max_member_size, is_separate_reminders)
+    return  Array.new(group_size) { Group.new(max_member_size) } if rest.zero?
+
+    if is_separate_reminders
+      Array.new(group_size - 1) { Group.new(max_member_size) } + [Group.new(rest)]
+    else
+      min_group_size = max_member_size - rest
+      Array.new(group_size - min_group_size) { Group.new(max_member_size) } +
+        Array.new(min_group_size) { Group.new(max_member_size - 1) }
+    end
   end
 end
--- lib/group_mixer/mixer.rb
+++ lib/group_mixer/mixer.rb
@@ -5,14 +5,10 @@ module GroupMixer
   class Mixer
     MAX_AMOUNT = 2 ** ([42].pack('i').size * 16 - 2) - 1
 
-    def initialize(people, past_set, group_size, is_separate_reminders)
+    def initialize(people, past_set, groups, is_separate_reminders)
       @people = people.shuffle
+      @groups = groups
       @past_groups = past_set.to_a.map { |s| s.is_a?(WeightedGroup) ? s : WeightedGroup.new(s) }
-      group_size = group_size.to_i
-      if group_size.zero?
-        raise ZeroGroupSize, 'group_size must be a number greater than 1'
-      end
-      @groups = make_groups(is_separate_reminders, group_size)
     end
 
     def execute
@@ -27,23 +23,6 @@ module GroupMixer
     end
 
     private
-
-    def make_groups(is_separate_reminders, group_size)
-      ps = @people.size
-      is_separate_reminders ?  separate_groups(group_size, ps) : flatting_groups(group_size, ps)
-    end
-
-    def flatting_groups(group_size, people_size)
-      min_member_size, max_group_size = people_size.divmod group_size
-      max_group_size.times.map { |n| Group.new(min_member_size + 1) } +
-          (group_size - max_group_size).times.map { |n| Group.new(min_member_size) }
-    end
-
-    def separate_groups(group_size, people_size)
-      member_size = people_size / (group_size - 1)
-      (group_size - 1).times.map { Group.new(member_size) } +
-        [Group.new(people_size % (group_size - 1))]
-    end
 
     def make_heuristic_from_past(people, past_set)
       past_set.each_with_object(Hash.new(0)) { |past, pheromone|
