9fdb8f9d3c89b8c0c7a75110fd425dab00d83f91
iwtn <ganta.viii@gmail.com>
2018-07-29 21:41:42 +0900

    add the same group in the past spec

--- spec/group_mixer/past_group_checker_spec.rb
+++ spec/group_mixer/past_group_checker_spec.rb
@@ -8,5 +8,39 @@ RSpec.describe GroupMixer::PastGroupChecker do
     subject { described_class.past_groups(groups, past_group_sets) }
 
     it { is_expected.to eq({}) }
+
+    context 'with past groups' do
+      let(:groups) {
+        Set.new([
+          Set.new([1, 2, 3]),
+          Set.new([4, 5, 6]),
+          Set.new([7, 8, 9]),
+        ])
+      }
+      let(:past_group_sets) {
+        Set.new([
+          Set.new([1, 4, 7]),
+          Set.new([2, 5, 8]),
+          Set.new([3, 6, 9]),
+        ])
+      }
+
+      it { is_expected.to eq({}) }
+
+      context 'the same group in the past' do
+        let(:past_group_sets) {
+          Set.new([
+            Set.new([1, 2, 9]),
+          ])
+        }
+
+        it {
+          is_expected.to eq({
+            1 => [ { member: 2, group: Set.new([1, 2, 9]) } ],
+            2 => [ { member: 1, group: Set.new([1, 2, 9]) } ],
+          })
+        }
+      end
+    end
   end
 end
